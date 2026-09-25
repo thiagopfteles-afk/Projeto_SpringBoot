c0a820886403c3bc68940c4de938d3c89d8b5ee0
Thigas.TT <thiagopfteles@gmail.com>
2026-09-25 15:39:20 -0300

    Commit incial!

---
+++ meu-projeto-springbot/src/main/java/com/produtoapi/MeuProjetoSpringbotApplication.java
@@ -0,0 +1,13 @@
+package com.produtoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeuProjetoSpringbotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeuProjetoSpringbotApplication.class, args);
+	}
+
+}
---
+++ meu-projeto-springbot/src/main/java/com/produtoapi/model/Produto.java
@@ -0,0 +1,74 @@
+package com.produtoapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.NotEmpty; 
+
+@Entity
+
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy =
+GenerationType.AUTO)
+	
+	private long id;
+	@NotEmpty (message= "informe o nome.")
+	private String nome;
+	private int quantidade;
+	private double preco;
+	private String status;
+
+	public Produto(String nome, int quantidade, double preco, String status) {
+		this.nome = nome;
+		this.quantidade= quantidade;
+		this.preco = preco;
+		this.status = status;
+	}
+
+	public Produto(){    // construtor vazio para criar um objeto 
+
+	}
+
+	public long getId(){
+		return id;
+	}
+
+	public void setId(long id){
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome){
+		this.nome = nome;
+	}
+
+	public int getQuantidade() {
+		return quantidade;
+	}
+
+	public void setQuantidade(int quantidade) {
+		this.quantidade= quantidade;
+	}
+
+	public double getPreco(){
+		return preco;
+	}
+
+	public void setPreco(Double preco) {
+		this.preco = preco;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public String getStatus(){
+		return status;
+		}
+
+}	
